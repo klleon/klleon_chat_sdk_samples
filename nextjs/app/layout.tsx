9438bdb37cf18fc87e8d5844f936c9fb3031a0e0
jihun.bang <jihun.bang@klleon.io>
2024-12-09 10:21:36 +0900

    fix: version

--- nextjs/app/layout.tsx
+++ nextjs/app/layout.tsx
@@ -7,7 +7,7 @@ export default function Layout({children}: { children: ReactNode }) {
             <meta charSet="UTF-8"/>
             <meta name="viewport" content="width=device-width, initial-scale=1"/>
             <title>Klleon Chat SDK</title>
-            <script src="https://sdk.klleon.io/klleon-chat/0.11.1/klleon_chat_sdk.js" defer/>
+            <script src="https://sdk.klleon.io/klleon-chat/0.12.0/klleon_chat_sdk.js" defer/>
         </head>
         <body style={{margin: 0}}>
             {children}
